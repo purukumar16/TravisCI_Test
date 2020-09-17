21235083ac58af21a96bd1e1e3a4ad04b138dd0f
purukumar16 <70796056+purukumar16@users.noreply.github.com>
2020-09-18 05:05:18 +0530

    With Ut

---
+++ ShrtUrlTest/Fribase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace GoogleDynamicLinksTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = "AIzaSyANe_GmoAnwzouoJ1aRPAxobiVK3wRSdfA";
+            string url = "https://google.com?id=123";
+
+            Shorten(apiKey, url);
+        }
+
+        public static void Shorten(string key, string url)
+        {
+            string post = "{\"longDynamicLink\": \"https://testdl.page.link/?link=" + url + "\"}";
+            string shortUrl = url;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + key);
+
+            try
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+
+                request.ServicePoint.Expect100Continue = false;
+                request.Method = "POST";
+                request.ContentLength = post.Length;
+                request.ContentType = "application/json";
+                request.Headers.Add("Cache-Control", "no-cache");
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    byte[] postBuffer = Encoding.ASCII.GetBytes(post);
+                    requestStream.Write(postBuffer, 0, postBuffer.Length);
+                }
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        using (StreamReader responseReader = new StreamReader(responseStream))
+                        {
+                            string json = responseReader.ReadToEnd();
+                            shortUrl = Regex.Match(json, @"""shortLink"": ?""(?<shortLink>.+)""").Groups["shortLink"].Value;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            Console.WriteLine("\nShortened URL: " + shortUrl);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShrtUrlTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ShrtUrlTest
+{
+    public class Program
+    {
+        public string GoogleAPIkey = "AIzaSyANe_GmoAnwzouoJ1aRPAxobiVK3wRSdfA";
+        static void Main(string[] args)
+        {
+            string shorturl = ShortUrl1();
+
+            Console.WriteLine(shorturl);
+            Console.ReadLine();
+        }
+
+        public static string ShortUrl1()
+        {
+            string shorturl;
+            string GoogleAPIkey = "AIzaSyANe_GmoAnwzouoJ1aRPAxobiVK3wRSdfA";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + GoogleAPIkey);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            string myFBDomain = "https://myappurl.page.link/?link=";
+
+            string longUrl = myFBDomain + "https://www.youtube.com/watch?v=Sjm7cZmwK98";
+
+            // string longurl = "https://YOURDOMAIN.page.link/?link=https://www.YOURLONGURL.com/account?g=sdasdhaskjdh4547576aksgskjg";
+
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"longDynamicLink\":\"" + longUrl + "\"}";
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+                dynamic data = JObject.Parse(responseText);
+                shorturl = data.shortLink;
+            }
+
+            return shorturl;
+        }
+    }
+}
